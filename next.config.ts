5b57222565ea495db02279b5302207714efc4d65
Orizu <michael@limpiar.online>
2025-03-19 15:39:40 +0100

    new

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next"
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-}
+  reactStrictMode: true,
+  trailingSlash: false, // Prevents Netlify from adding unnecessary slashes
+  output: "standalone", // Ensures everything needed is bundled
+  webpack: (config) => {
+    return config;
+  },
+};
 
-export default nextConfig
-
+export default nextConfig;
